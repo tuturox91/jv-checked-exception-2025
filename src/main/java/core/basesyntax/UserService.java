2535fae8b552d8f7233c8dcb9f0ca670035c970e
Sniklz <igorbukur@gmail.com>
2025-06-19 15:58:19 +0200

    test

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        //write your code here test
     }
 
     public void saveUser(User user) {
